b82cdac975ba02db9f2f957a313294e341d44143
simo9000 <alkylate2000@gmail.com>
2014-11-16 17:03:45 -0800

    the extra stuff

---
+++ dbBuild.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS tblUsers;
+DROP TABLE IF EXISTS tblMonitoredPages;
+DROP TABLE IF EXISTS tblNotificationRequests;
+DROP TABLE IF EXISTS tblModificationLog;
+
+
+CREATE TABLE tblUsers(
+	pk_id INT not null,
+	fdEmailAddress TEXT not null
+);
+
+CREATE TABLE tblMonitoredPages(
+	pk_id INT not null,
+	fdURL TEXT not null,
+	fdHash BLOB,
+	fdLastUpdate TEXT,
+	fdProcessFlag INT not null
+);
+
+CREATE TABLE tblNotificationRequests(
+	FK_USER_ID INT NOT NULL,
+	FK_PAGE_ID INT NOT NULL,
+	fdActive INT NOT NULL
+);
+
+CREATE TABLE tblModificationLog(
+	FK_PAGE_ID INT NOT NULL,
+	fdUpdateTime TEXT NOT NULL
+);

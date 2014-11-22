c73747d61706159baf09ac8dc894935ed778b514
scottmc500 <smm178@case.edu>
2014-11-22 09:33:01 -0500

    Database/Email File and Test File
    I added a file which both retrieves data from the pageMonitor database
    and sends it to a user via email.

--- java/pageMonitor2/EmailNotifier.java
+++ java/pageMonitor2/EmailNotifier.java
@@ -1,16 +1,56 @@
+import java.io.File;
 import java.util.Properties;
+
 import javax.mail.*;
 import javax.mail.internet.*;
+
 import java.util.Observable;
 import java.util.Observer;
+import java.sql.*;
 
 public class EmailNotifier implements Observer {
 	private String to;
 	private String from;
 	
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		to = "khertel@ursuline.edu";
+	public void printFileList(String path) {
+		File folder = new File(path);
+		File[] filelist = folder.listFiles();
+		for (File f : filelist) {
+			System.out.println(f.getName());
+		}
+	}
+	
+	public String databaseStuff() {
+		Connection c = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:pageMonitor.db");
+			c.setAutoCommit(false);
+			System.out.println("Connected Successfully");
+			Statement stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM tblMonitoredPages;");
+			StringBuffer message = new StringBuffer();
+			while (rs.next()) {
+				String pk_id = rs.getString("pk_id");
+				String fdURL = rs.getString("fdURL");
+				String fdHash = rs.getString("fdHash");
+				String fdLastUpdate = rs.getString("fdLastUpdate");
+				String fdProcessFlag = rs.getString("fdProcessFlag");
+				message.append("PK_ID = " + pk_id + "\n");
+				message.append("fdURL = " + fdURL + "\n");
+				message.append("fdHASH = " + fdHash + "\n");
+				message.append("fdLASTUPDATE = " + fdLastUpdate + "\n");
+				message.append("fdPROCESSFLAG = " + fdProcessFlag + "\n\n");
+			}
+			message.append("Best regards,\nScott McHenry");
+			return message.toString();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void emailStuff() {
+		to = "smm178@case.edu";
 		from = "smchenry2014@gmail.com";
 		String username = "smchenry2014@gmail.com";
 		String password = "Mcwilak5";
@@ -30,18 +70,17 @@ public class EmailNotifier implements Observer {
 			message.setFrom(new InternetAddress(from));
 			message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
 			message.setSubject("This is a Test Email");
-			message.setText("Hey Simon,\n"
-					+ "If you are receiving this email, it means that I have successfully"
-					+ " gotten JavaMail to work properly.  I got it to work a few times"
-					+ " already by sending emails to my old college email, so I'm pretty"
-					+ " confident about this test.  Please email me back if you get this"
-					+ " so that I know we're good to go on at least one front.\n\n"
-					+ "Best regards,\nScott McHenry\n\n"
-					+ "P.S.  I used Google's SMTP service to make this work.");
+			String mytext = databaseStuff();
+			message.setText(mytext);
 			Transport.send(message);
 			System.out.println("Message sent successfully...");
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	@Override
+	public void update(Observable arg0,Object arg1) {
+		emailStuff();
 	}
 }
---
+++ java/pageMonitor2/Test_EmailNotifier.java
@@ -0,0 +1,7 @@
+
+public class Test_EmailNotifier {
+	public static void main(String[] args) {
+		EmailNotifier mytest = new EmailNotifier();
+		mytest.emailStuff();
+	}
+}
